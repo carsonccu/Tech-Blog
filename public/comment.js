d401e186912ef9df99786ff318f7c199582fbe9a
Carson <carsoncolgateccu@gmail.com>
2022-03-10 18:25:40 -0700

    added login logout function and post

---
+++ public/comment.js
@@ -0,0 +1,27 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_content = document.querySelector('#comment-content').value;
+
+    if (comment_content) {
+
+        const response = await fetch(`/api/posts/comment`, {
+            method: 'POST',
+            body: JSON.stringify({ comment_content }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to create comment');
+        }
+    }
+};
+
+
+document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newFormHandler);
