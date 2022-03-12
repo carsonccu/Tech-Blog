ca946f3a10fed618d7312c9e61872bfefae3ae1d
Carson <carsoncolgateccu@gmail.com>
2022-03-11 20:35:53 -0700

    having port issues

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
